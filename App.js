6453cb76cbe41e36323b053245e4d378e3f0f1a4
Kmiles <kmilesstem2023@gmail.com>
2023-10-21 17:45:21 -0400

    SignIn 1

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { React } from 'react';
+import { NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignUp from './Screens/SignUp';
+
+const stack = createNativeStackNavigator;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    //<View><Text>Hello</Text></View>
+    <SignUp/>
+  //  <NavigationContainer>
+  //   <stack.Navigator>
+  //     <stack.Screen name = "Sign Up" component={SignUp} options={{headerShown: false}}/>
+  //   </stack.Navigator>
+  //  </NavigationContainer>
   );
 }
+// Navigation.registerComponent('Sign Up', () => SignUp);
+// Navigation.events().registerAppLaunchedListener(async () => {
+//   Navigation.setRoot({
+//     root: {
+//       stack: {
+//         children: [
+//           {
+//             component: {
+//               name: 'Sign Up'
+//             }
+//           }
+//         ]
+//       }
+//     }
+//   });
+// });
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ Screens/SignUp.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import {Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
+
+
+export default function SignUp({navigation}){
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    return(
+        <View style={styles.container}>
+            <View style={{marginHorizontal: 20}}>
+                <Text style={styles.headers}>Email</Text>
+                <TextInput style={styles.input} value={email} onChangeText={(text) => setEmail(text)} />
+                <Text style={styles.headers}>Name</Text>
+                <TextInput style={styles.input} value={name} onChangeText={(text) => setName(text)}/>
+                <Text style={styles.headers}>Password</Text>
+                <TextInput style={styles.input} value={password} onChangeText={(text) => setPassword(text)} secureTextEntry={true} />
+                <Pressable style={styles.button} onPress={alert(email + " " + name + " " + password)}>Sign Up</Pressable>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    input: {
+        marginBottom: 30,
+        height: 50,
+        backgroundColor: "#C5E2FF",
+        justifyContent: 'center',
+        padding: 16,
+        fontSize: 20
+    },
+    container: {
+        justifyContent: 'center',
+        flex: 1,
+        backgroundColor: "#BFCEB8"
+    },
+    headers: {
+        fontSize: 24,
+        marginBottom: 15
+    },
+    button: {
+        marginTop: 24,
+        fontSize: 20,
+        backgroundColor: "#BFCEB8",
+        justifyContent: "center"
+    }
+});
