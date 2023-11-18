9d737b4d597c267bf929229d3a9328517955c606
Kmiles <kmilesstem2023@gmail.com>
2023-11-17 20:13:24 -0500

    potential solution

--- providers/AuthProviders.js
+++ providers/AuthProviders.js
@@ -1,4 +1,28 @@
+import React, { useContext, useState, useEffect, useRef } from "react";
+import Realm from "realm";
+import app from "../RealmApp";
+
 const AuthContext = React.createContext(null);
+
+//commented out for now
+// const AuthProvider = ({ children }) =>{
+//     const [user, setUser] = useState(app.currentUser);
+//     const realmRef = useRef(null);
+
+//     useEffect(() => {
+//         if(!user){
+//             console.warn("NO USER");
+//             return;
+//         }
+
+//         const config = {
+//             sync: {
+//                 user,
+//                 partitionValue: `user=${user.id}`,
+//             }
+//         }
+//     });
+//}
 
 const login = async(email, password) =>{
     const creds = Realm.Credentials.emailPassword();
