e4ae9497a2363db1b655d519c14aad462f7c1d4f
Grace Cai <grace.cai7@gmail.com>
2024-02-01 10:30:32 -0500

    init camera

---
+++ Screens/Camera.js
@@ -0,0 +1,8 @@
+function Camera() {
+	const device = useCameraDevice("back");
+
+	if (device == null) return <NoCameraErrorView />;
+	return (
+		<Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+	);
+}
